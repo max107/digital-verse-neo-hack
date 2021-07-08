bed28f310aaf8a766e3194dc03e024408e74bbe2
Panich Maxim <panichmax@gmail.com>
2021-07-08 19:53:27 +0300

    Backup

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # TestNet Wallet:
-1. dv.neo-wallet.json
+1. 
+wallets/dv.neo-wallet.json
 NSadsnNbMKd5DDdLESNFUZwxr9Zmyc9wBJ
 qwerty
 
+# Deployed smart contracts
+1. 
+NSadsnNbMKd5DDdLESNFUZwxr9Zmyc9wBJ
+NEO N3 TestNet:0
+0xe8fdde2574ec1f0a2660eccadfb588c4fc1fa55ba02b8dca633747bdaf9c933f - transaction
+https://neo3.neotube.io/contract/0x9b437260ae6a5938a858f66dd802fc399ec128df - info
+
 # TestNet Tools
+
+https://dora.coz.io
+https://neo3.neotube.io/
+
+
 https://neoscan-testnet.io/address/AStJyBXGGBK6bwrRfRUHSjp993PB5C9QgF
 https://neowish.ngd.network/#/
--- server/main.go
+++ server/main.go
@@ -1,6 +1,83 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/joeqian10/neo3-gogogo/rpc"
+	"github.com/joeqian10/neo3-gogogo/wallet"
+	"github.com/joeqian10/neo3-gogogo/crypto"
+	"github.com/joeqian10/neo3-gogogo/helper"
+	"github.com/joeqian10/neo3-gogogo/rpc"
+	"github.com/joeqian10/neo3-gogogo/sc"
+	"github.com/joeqian10/neo3-gogogo/tx"
+	"github.com/joeqian10/neo3-gogogo/wallet"
+	"github.com/gin-gonic/gin"
+)
+
+func mint() (hash String, err error) {
+	port := "http://seed1t.neo.org:20332"
+	client := rpc.NewClient(yourPort)
+
+	var magic uint32 = 844378958 // change to your network magic number
+	ps := helper.ProtocolSettings{
+		Magic:          magic,
+		AddressVersion: helper.DefaultAddressVersion,
+	}
+	w, err := wallet.NewNEP6Wallet("./dv.neo-wallet.json", &ps, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	err = w.Unlock("qwerty")
+	if err != nil {
+		return nil, err
+	}
+
+	// create a WalletHelper
+	wh := wallet.NewWalletHelperFromWallet(client, w)
+
+	// build script
+	scriptHash, err := helper.UInt160FromString("0x9b437260ae6a5938a858f66dd802fc399ec128df")
+	if err != nil {
+		return nil, err
+	}
+	// if your contract method has parameters
+	cp1 := sc.ContractParameter{
+		Type:  sc.ByteArray,
+		Value: []byte{},
+	}
+	script, err := sc.MakeScript(scriptHash, "mint", []interface{}{cp1})
+	if err != nil {
+		return nil, err
+	}
+
+	// get balance of gas in your account
+	balancesGas, err := wh.GetAccountAndBalance(tx.GasToken)
+	if err != nil {
+		return nil, err
+	}
+
+	// make transaction
+	trx, err := wh.MakeTransaction(script, nil, []tx.ITransactionAttribute{}, balancesGas)
+	if err != nil {
+		return nil, err
+	}
+
+	// sign transaction
+	trx, err = wh.SignTransaction(trx, magic)
+	if err != nil {
+		return nil, err
+	}
+
+	// send the transaction
+	rawTxString := crypto.Base64Encode(trx.ToByteArray())
+	response := wh.Client.SendRawTransaction(rawTxString)
+	if response.HasError() {
+		// do something
+		return nil, errors.New("Send transaction error")
+	}
+
+	// hash is the transaction hash
+	hash := trx.GetHash().String()
+	return hash, nil
+}
 
 func main() {
 	r := gin.Default()
@@ -20,5 +97,14 @@ func main() {
 			"nick":    nick,
 		})
 	})
+
+	r.GET("/mint", func(c *gin.Context) {
+		tx_hash, err := mint()
+		c.JSON(200, gin.H{
+			"tx_hash": tx_hash,
+			"error": err,
+		})
+	})
+
 	r.Run()
 }
